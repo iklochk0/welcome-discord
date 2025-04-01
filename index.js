a21434b468c2a9848bdd2d194f672462d5e3fb3f
iklochk0 <61226205+iklochk0@users.noreply.github.com>
2025-04-01 18:48:19 +0200

    index.js aktualisieren

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // Register Open Sans font
-registerFont(path.join(__dirname, 'OpenSans-Regular.ttf'), {
+registerFont(path.join(__dirname, "fonts", 'OpenSans-Regular.ttf'), {
   family: 'OpenSans'
 });
 
